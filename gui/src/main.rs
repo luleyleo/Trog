4e5358262c164beae40b9141a5c4c691134ca26f
Leopold Luley <git@leopoldluley.de>
2022-08-07 10:15:45 +0200

    Add second list and bind window control visibility.

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -52,16 +52,21 @@ fn load_resources(_app: &adw::Application) {
 }
 
 fn setup(app: &adw::Application) {
+    let leaflet = adw::Leaflet::new();
+    leaflet.append(&list_channels(&leaflet));
+    leaflet.append(&gtk::Separator::new(gtk::Orientation::Vertical));
+    leaflet.append(&list_items(&leaflet));
+
     let window = adw::ApplicationWindow::builder()
         .application(app)
         .default_width(350)
         .default_height(600)
-        .content(&list())
+        .content(&leaflet)
         .build();
     window.show();
 }
 
-fn list() -> impl IsA<gtk::Widget> {
+fn list_channels(leaflet: &adw::Leaflet) -> impl IsA<gtk::Widget> {
     let row = adw::ActionRow::builder()
         .activatable(true)
         .title("Click me")
@@ -90,6 +95,9 @@ fn list() -> impl IsA<gtk::Widget> {
     header.pack_start(&add_button);
     header.pack_end(&menu_button);
     header.pack_end(&refresh_button);
+    leaflet
+        .bind_property("folded", &header, "show-end-title-buttons")
+        .build();
 
     let list = gtk::ListBox::builder()
         .selection_mode(gtk::SelectionMode::None)
@@ -150,7 +158,113 @@ fn list() -> impl IsA<gtk::Widget> {
         .vexpand(true)
         .build();
 
-    let content = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    let content = gtk::Box::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .hexpand(true)
+        .build();
+    content.append(&header);
+    content.append(&scrolled_list);
+
+    content
+}
+
+fn list_items(leaflet: &adw::Leaflet) -> impl IsA<gtk::Widget> {
+    let row = adw::ActionRow::builder()
+        .activatable(true)
+        .title("Click me")
+        .build();
+    row.connect_activated(|_| {
+        eprintln!("Clicked!");
+    });
+
+    let add_button = gtk::Button::builder()
+        .icon_name("list-add-symbolic")
+        .build();
+    let menu_button = gtk::Button::builder()
+        .icon_name("open-menu-symbolic")
+        .build();
+    let refresh_button = gtk::Button::builder()
+        .icon_name("view-refresh-symbolic")
+        .build();
+
+    let model = fetch_model();
+    let title = gtk::Label::builder()
+        .label(&model.title())
+        .css_classes(vec!["title".into()])
+        .build();
+
+    let header = adw::HeaderBar::builder().title_widget(&title).build();
+    header.pack_start(&add_button);
+    header.pack_end(&menu_button);
+    header.pack_end(&refresh_button);
+    leaflet
+        .bind_property("folded", &header, "show-end-title-buttons")
+        .flags(BindingFlags::INVERT_BOOLEAN)
+        .build();
+
+    let list = gtk::ListBox::builder()
+        .selection_mode(gtk::SelectionMode::None)
+        .build();
+
+    list.bind_model(Some(&model.items()), |item| {
+        let item = item.clone().downcast::<storage::Item>().unwrap();
+
+        let title = gtk::Label::builder()
+            .label(&item.title())
+            .use_markup(false)
+            .css_classes(vec!["title".to_string()])
+            .wrap(true)
+            .build();
+
+        let toggle = gtk::CheckButton::builder()
+            .css_classes(vec!["read-toggle".into()])
+            .build();
+        toggle
+            .bind_property("active", &item, "read")
+            .flags(BindingFlags::BIDIRECTIONAL)
+            .build();
+
+        let content = gtk::Box::builder()
+            .orientation(gtk::Orientation::Horizontal)
+            .spacing(8)
+            .halign(gtk::Align::Start)
+            .margin_top(8)
+            .margin_end(12)
+            .margin_bottom(8)
+            .margin_start(12)
+            .build();
+
+        content.append(&toggle);
+        content.append(&title);
+
+        let row = adw::ActionRow::builder()
+            .activatable(true)
+            .child(&content)
+            .build();
+
+        row.connect_activated(move |row| {
+            let window = row
+                .root()
+                .unwrap()
+                .downcast::<adw::ApplicationWindow>()
+                .unwrap();
+            gtk::show_uri(Some(&window), &item.link(), gtk::gdk::CURRENT_TIME);
+            item.set_read(true);
+        });
+
+        row.upcast()
+    });
+
+    let scrolled_list = gtk::ScrolledWindow::builder()
+        .hscrollbar_policy(gtk::PolicyType::Never)
+        .child(&list)
+        .vexpand(true)
+        .build();
+
+    let content = gtk::Box::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .hexpand(true)
+        .build();
     content.append(&header);
     content.append(&scrolled_list);
 
